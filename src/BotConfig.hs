22cb757478ca86306e43b2a99295e1d00d635f6e
Cameron Ramsell (GlitchedTurtle) <ramsellcameron@gmail.com>
2022-03-25 06:24:51 +0000

    Updates to mongo store

--- src/BotConfig.hs
+++ src/BotConfig.hs
@@ -2,11 +2,38 @@
 module BotConfig where
 
 import Data.Text (Text)
+import Database.MongoDB (Database, Username, Password, PortID (PortNumber))
 
 cfDiscordToken :: Text
-cfMongoHost :: String
 cfLeaderboardSize :: Int
+cfMongoHost :: String
+cfMongoPort :: PortID
+cfMongoDb :: Database 
+cfMongoDoAuth :: Bool
+cfMongoUseTLS :: Bool
+cfMongoUsername :: Username 
+cfMongoPassword :: Password 
 
 cfDiscordToken = "OTU0NDMxOTg0MTM3MDM1OTA2.YjTCDQ.RwhUfmQnjm6qQCrmSidJvdObQps"
-cfMongoHost = "127.0.0.1"
 cfLeaderboardSize = 10
+
+-- the mongo library is terrible
+-- to connect to atlas you have to jump through a bunch of hoops
+
+-- use this for pre-created atlas instance (feel free to connect yourself and modify stuff)
+cfMongoHost = "countbot-shard-00-01.6czpo.mongodb.net"
+cfMongoPort = PortNumber 27017
+cfMongoDb   = "countbot"
+cfMongoDoAuth = True
+cfMongoUseTLS = True
+cfMongoUsername = "auth"
+cfMongoPassword = "m7qRzrffAmuF3FR"
+
+-- use this for a local instance of mongo
+{-cfMongoHost = "127.0.0.1"
+cfMongoPort = PortNumber 27017
+cfMongoDb   = "countbot"
+cfMongoDoAuth = False
+cfMongoUseTLS = False
+cfMongoUsername = ""
+cfMongoPassword = ""-}
--- src/store/MongoStore.hs
+++ src/store/MongoStore.hs
@@ -3,8 +3,9 @@ module MongoStore where
 
 import Discord.Types (UserId)
 import Database.MongoDB
+import qualified Database.MongoDB.Transport.Tls as DBTLS
 import Control.Monad (void)
-import Control.Exception (catch, Exception)
+import Control.Exception (catch, Exception, try, SomeException (SomeException))
 import Data.Maybe (isJust)
 import Data.Text (Text, pack)
 import BotHandler ( readMongo, writeMongo, BotHandler )
@@ -15,7 +16,8 @@ import ProfileData
 import Request (RequestScope, scopeSerialize)
 import GHC.IO (catchAny)
 import Control.Concurrent (forkIO)
-import BotConfig (cfMongoHost)
+import BotConfig (cfMongoDoAuth, cfMongoUsername, cfMongoPassword, cfMongoDb, cfMongoHost, cfMongoPort, cfMongoUseTLS)
+import GHC.IO.Exception (IOException(IOError))
 
 mong :: Action IO a -> BotHandler a
 mong action = do
@@ -24,13 +26,38 @@ mong action = do
         Nothing -> do
             newCon <- liftIO mongoMakeConnection
             writeMongo newCon
+
             return newCon
         Just con -> return con
 
-    liftIO $ access pipe master "statbot" action
+    res <- liftIO $ try $ access pipe master cfMongoDb action
+    case res of
+        Left e -> do -- if theres an error, we want to output it
+            let e' = e :: SomeException
+            liftIO $ putStrLn ("[Mongo] Error while executing: " ++ show e) -- output the error
+
+            error "Error"
+        Right ret -> return ret
 
 mongoMakeConnection :: IO Pipe
-mongoMakeConnection = connect (host cfMongoHost)
+mongoMakeConnection = do
+    putStrLn "[Mongo] Attempting connection to mongo server..."
+    dbRes <- try $ if cfMongoUseTLS then 
+        DBTLS.connect cfMongoHost cfMongoPort
+        else connect $ Host cfMongoHost cfMongoPort
+    case dbRes of
+        Left e -> do -- if theres an error while connecting
+            let e' = e :: SomeException
+            liftIO $ putStrLn ("[Mongo] Error while connecting: " ++ show e) -- output the error
+
+            error "Error connecting..."
+        Right pipe -> do
+            putStrLn "[Mongo] Connection established.."
+            when cfMongoDoAuth $ do -- if we need to authenticate, do it
+                putStrLn "[Mongo] Attempting authentication..."
+                void $ access pipe UnconfirmedWrites "admin" (auth cfMongoUsername cfMongoPassword)
+                putStrLn "[Mongo] Authenticated"
+            return pipe
 
 mongoFetchProfile :: UserId -> BotHandler (Maybe ProfileData)
 mongoFetchProfile userId = fmap deserialize <$>
